e1883014fd4bedc6635a83dec17182b0731134f5
mandeep147 <mndpkaur14@gmail.com>
2017-07-30 16:34:31 -0700

    ch-1 arrays and strings

---
+++ src/edu/mandeep/ctci/IsUnique.java
@@ -0,0 +1,46 @@
+/**
+ * Determine if string has all unique characters
+ */
+package edu.mandeep.ctci;
+
+/**
+ * @author mandeep
+ *
+ */
+public class IsUnique {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "aaabbfde";
+		String str1 = "abcdefghijk";
+		String str2 = "qwo[pknahsjmczl;./WPKLQAM";
+		System.out.println("String "+ str +"contains unique characters: "+ isUniqueChars(str));
+		System.out.println("String "+ str1 +"contains unique characters: "+ isUniqueChars(str1));
+		System.out.println("String "+ str2 +"contains unique characters: "+ isUniqueChars(str2));
+	}
+	//assumption: ASCII string
+	static boolean isUniqueChars(String str){
+		
+		//if length of string is more than 128 it has duplicate values
+		if(str.length() > 128)
+			return false;
+		
+		//array of 128 chars
+		boolean[] flag = new boolean[128];
+		
+		for(int i = 0; i < str.length(); i++){
+			int val = str.charAt(i);
+			
+			//if ith index is previously set, duplicate entry of same char is encountered
+			if(flag[val])
+				return false;
+			flag[val] = true;
+		}
+		
+		return true;
+	}
+
+}
