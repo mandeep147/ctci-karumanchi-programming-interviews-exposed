bda8c3f218f79e8348ed8a1c0ac7db60deca8a0e
mandeep147 <mndpkaur14@gmail.com>
2017-08-30 02:32:40 -0700

    BST

---
+++ src/edu/mandeep/karumanchi/trees/BinarySearchTree.java
@@ -0,0 +1,50 @@
+
+/**
+ *incomplete 
+ */
+package edu.mandeep.karumanchi.trees;
+
+/**
+ * @author mandeep
+ *
+ */
+public class BinarySearchTree {
+	
+	class Node {
+        int key;
+        Node left, right;
+ 
+        public Node(int item) {
+            key = item;
+            left = right = null;
+        }
+    }
+ 
+    Node root;
+ 
+    public BinarySearchTree() { 
+        root = null; 
+    }
+	
+	//insert an element 
+	public TreeNode Insert(TreeNode root, int data){
+		if(root == null){
+			root = new TreeNode();
+			root.setData(data);
+			root.setLeft(null);
+			root.setRight(null);
+		}else{
+			if(data < root.getData())
+				root.setLeft(Insert(root.getLeft(), data));
+			else if(data > root.getData())
+				root.setRight(Insert(root.getRight(), data));
+		}
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		BinarySearchTree tree = new BinarySearchTree();
+		
+		//tree.Insert(tree, 50);
+	}
+}
