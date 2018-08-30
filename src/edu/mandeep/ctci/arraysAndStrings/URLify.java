6b45da9f9d059ce1e7b85c566526b23be0df548e
mandeep147 <mndpkaur14@gmail.com>
2018-08-29 20:08:53 -0700

    URLify

--- src/edu/mandeep/ctci/arraysAndStrings/URLify.java
+++ src/edu/mandeep/ctci/arraysAndStrings/URLify.java
@@ -42,20 +42,21 @@ public class URLify {
 		
 		//final length will be truelength + twice the spaces
 		index = trueLength + 2 * spaces;
+		char[] res = new char[index];
 		if(trueLength < str.length)
 			str[trueLength] = '\0';
 		
 		for( i = trueLength - 1; i >= 0; i--){
 			if(str[i] == ' '){
-				str[index - 1] = '0';
-				str[index - 2] = '2';
-				str[index - 3] = '%';
+				res[index - 1] = '0';
+				res[index - 2] = '2';
+				res[index - 3] = '%';
 				index = index - 3;
 			}else{
-				str[index - 1] = str[i];
+				res[index - 1] = str[i];
 				index--;
 			}
 		}
-		return str;
+		return res;
 	}
 }
